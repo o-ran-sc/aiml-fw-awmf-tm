b4fd6543ba96f5d1bcfe1fb890c4534feff1d924
rajdeep11 <rajdeep.sin@samsung.com>
2023-05-11 14:46:29 +0530

    Unit test for create_feature_group
    Issue-Id: AIMLFW-45
    
    Change-Id: I2abd10f791218cda2adaff7526ed5a623e71a992
    Signed-off-by: rajdeep11 <rajdeep.sin@samsung.com>

--- tests/test_trainingmgr_operations.py
+++ tests/test_trainingmgr_operations.py
@@ -85,3 +85,42 @@ class Test_upload_pipeline:
                                     content_type="application/json")
         assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
         assert expected_data in response.json.keys() 
+
+class Test_create_dme_filtered_data_job:
+
+    the_response=Response()
+    the_response.status_code=status.HTTP_201_CREATED
+    @patch('trainingmgr.common.trainingmgr_operations.requests.put', return_value=the_response)
+    def test_create_dme_filtered_data_job(self, mock1):
+        mocked_TRAININGMGR_CONFIG_OBJ=mock.Mock(name="TRAININGMGR_CONFIG_OBJ")
+        attrs_TRAININGMGR_CONFIG_OBJ = {'kf_adapter_ip.return_value': '123', 'kf_adapter_port.return_value' : '100'}
+        mocked_TRAININGMGR_CONFIG_OBJ.configure_mock(**attrs_TRAININGMGR_CONFIG_OBJ)
+        source_name=""
+        db_org=""
+        bucket_name=""
+        token=""
+        features=[]
+        feature_group_name="test"
+        host="10.0.0.50"
+        port="31840"
+        response=trainingmgr_operations.create_dme_filtered_data_job(mocked_TRAININGMGR_CONFIG_OBJ, source_name, db_org, bucket_name, token, features, feature_group_name, host, port)
+        assert response.status_code==201, "create_dme_filtered_data_job failed"
+
+    @patch('trainingmgr.common.trainingmgr_operations.create_url_host_port', side_effect=Exception("Mocked Error"))
+    def test_negative_create_dme_filtered_data_job(self, mock1):
+        mocked_TRAININGMGR_CONFIG_OBJ=mock.Mock(name="TRAININGMGR_CONFIG_OBJ")
+        attrs_TRAININGMGR_CONFIG_OBJ = {'kf_adapter_ip.return_value': '123', 'kf_adapter_port.return_value' : '100'}
+        mocked_TRAININGMGR_CONFIG_OBJ.configure_mock(**attrs_TRAININGMGR_CONFIG_OBJ)
+        source_name=""
+        db_org=""
+        bucket_name=""
+        token=""
+        features=[]
+        feature_group_name="test"
+        host="10.0.0.50"
+        port="31840"
+        try:
+            response=trainingmgr_operations.create_dme_filtered_data_job(mocked_TRAININGMGR_CONFIG_OBJ, source_name, db_org, bucket_name, token, features, feature_group_name, host, port)
+            assert False
+        except Exception:
+            assert True
