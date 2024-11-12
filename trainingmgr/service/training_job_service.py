1df6d7f0f94c719b4841f1a8106c230f464f4425
subhash kumar singh <subh.singh@samsung.com>
2024-11-12 14:13:37 +0000

    Implement the basic implementation for delete TJ
    Implemented deletion of Training Job as per spec defined
    structure.
    TODO: Cancel the training job on delete request.
    
    Change-Id: I236c837907c01d784f59d2bfe299a898b94a0253
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>

---
+++ trainingmgr/service/training_job_service.py
@@ -0,0 +1,40 @@
+# ==================================================================================
+#
+#       Copyright (c) 2024 Samsung Electronics Co., Ltd. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ==================================================================================
+from trainingmgr.db.trainingjob_db import delete_trainingjob_by_id
+from trainingmgr.common.exceptions_utls import DBException
+
+def delete_training_job(training_job_id : int):
+    """
+    This function handles the service to delete the training job resource by id.
+    
+    Args:
+        training_job_id (int): id of training job.
+    
+    Returns:
+        bool: boolean to represent if the trainingjob is deleted.
+    
+    Raises:
+        DBException: If there error during operation. 
+
+    """
+    try:
+        #TODO: cancel training job from kubeflow training
+        return delete_trainingjob_by_id(id=training_job_id)
+    except Exception as err :
+        raise DBException(f"delete_trainining_job failed with exception : {str(err)}")
+
