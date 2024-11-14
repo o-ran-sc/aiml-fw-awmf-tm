0dc8b795378d97d9c3c4e1454978cbfc2457fb0c
subhash kumar singh <subh.singh@samsung.com>
2024-11-14 16:52:41 +0000

    Implement get operation
    Implemented get operation for training job by id.
    
    Change-Id: Iefa53aa484575ff72b9a0c83fc2269eb71a9660f
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>

--- trainingmgr/controller/trainingjob_controller.py
+++ trainingmgr/controller/trainingjob_controller.py
@@ -16,7 +16,7 @@
 #
 # ==================================================================================
 from flask import Blueprint, jsonify, request
-from trainingmgr.service.training_job_service import delete_training_job, create_training_job
+from trainingmgr.service.training_job_service import delete_training_job, create_training_job, get_training_job
 
 training_job_controller = Blueprint('training_job_controller', __name__)
 
@@ -44,3 +44,8 @@ def create_trainingjob():
     create_training_job(data)
     print(f'create traiing job: {data}')
     return '', 200
+
+@training_job_controller.route('/training-jobs/<int:training_job_id>', methods=['GET'])
+def get_trainingjob(training_job_id):
+    print(f'get the trainingjob correspoinding to id: {training_job_id}')
+    return jsonify(get_training_job(training_job_id)), 200
--- trainingmgr/db/trainingjob_db.py
+++ trainingmgr/db/trainingjob_db.py
@@ -319,3 +319,9 @@ def delete_trainingjob_by_id(id: int):
     except Exception as e:
         db.session.rollback()
         raise DBException(f'{DB_QUERY_EXEC_ERROR} : {str(e)}' )
+
+def get_trainingjob(id: int):
+    tj = db.session.query(TrainingJob).get(id)
+    print(f'{TrainingJobSchema().dump(tj)}')
+    return tj
+
