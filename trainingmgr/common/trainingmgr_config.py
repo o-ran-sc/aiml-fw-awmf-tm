254f29aef560539a69fce1e8c55c1fff86515924
subhash kumar singh <subh.singh@samsung.com>
2024-11-15 12:55:13 +0000

    Create TrainingConfig Singleton class
    Created TrainingConfig as singletone class to share cofiguraiton
    and logger
    
    e.g.
    def __init__():
      self.logger = TrainingConfig().__logger
    
    then use the `self.logger` for logging.
    
    Change-Id: I4eb5f38ffc288825fe6c9ac38312c99238150abe
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>

--- trainingmgr/common/trainingmgr_config.py
+++ trainingmgr/common/trainingmgr_config.py
@@ -29,6 +29,13 @@ class TrainingMgrConfig:
     This class conatains method for getting configuration varibles.
     """
 
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(TrainingMgrConfig, cls).__new__(cls)
+        return cls.__instance
+
     def __init__(self):
         """
         This constructor filling configuration varibles.
