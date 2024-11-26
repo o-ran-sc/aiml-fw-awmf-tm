d0c0db2a3c0635ee46ce3c64d51a38ab0a3baaea
rajdeep11 <rajdeep.sin@samsung.com>
2024-11-26 12:39:31 +0530

    functiom to update a field of a trainingjob
    Change-Id: I0139421ad71b5d6e0d6d1c0e8c5afd4b1a4b98da
    Signed-off-by: rajdeep11 <rajdeep.sin@samsung.com>

--- trainingmgr/db/trainingjob_db.py
+++ trainingmgr/db/trainingjob_db.py
@@ -221,7 +221,6 @@ def change_field_of_latest_version(trainingjob_name, field, field_value):
     """
     This function updates the field's value for given trainingjob.
     """
-
     try:
         trainingjob_max_version = TrainingJob.query.filter(TrainingJob.trainingjob_name == trainingjob_name).order_by(TrainingJob.version.desc()).first()
         if field == "notification_url":
@@ -231,7 +230,6 @@ def change_field_of_latest_version(trainingjob_name, field, field_value):
             trainingjob_max_version.run_id = field_value
             trainingjob_max_version.updation_time = datetime.datetime.utcnow()
         db.session.commit()
-
     except Exception as err:
         raise DBException("Failed to execute query in change_field_of_latest_version,"  + str(err))
     
@@ -274,6 +272,17 @@ def change_field_value_by_version(trainingjob_name, version, field, field_value)
             db.session.commit()
     except Exception as err:
         raise DBException("Failed to execute query in change_field_value_by_version," + str(err))
+     
+def change_field_value(traininigjob_id, field, value):
+    """
+    This function updates field's value to field_value of trainingjob.
+    """
+    try:
+        trainingjob = TrainingJob.query.filter(TrainingJob.id==traininigjob_id).one()
+        setattr(trainingjob, field, value)
+        db.session.commit()
+    except Exception as err:
+        raise DBException("Failed to execute query in change_field_value," + str(err))
 
 def delete_trainingjob_version(trainingjob_name, version):
     """
